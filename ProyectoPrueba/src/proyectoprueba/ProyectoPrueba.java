bbc23c60db6611817700f04c1d31e80261a231ee
Erick Sanchez <Erick Sanchez@Erick>
2016-09-01 10:19:03 -0500

    Segunda Modificacion ProyectoPrueba

--- ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
+++ ProyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package proyectoprueba;
 
+package proyectoprueba;
 /**
  *
  * @author Erick Sanchez
  */
 public class ProyectoPrueba {
+    
+    String nombre;
+    int valor;
 
     /**
      * @param args the command line arguments
@@ -17,5 +15,7 @@ public class ProyectoPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public String getNombre(){
+    return nombre;
+    }
 }
